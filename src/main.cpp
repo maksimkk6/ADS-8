3b75747af0a1d1c8f45fac6ad7517f881d0bda2f
maksimkk6 <konnovmaks@bk.ru>
2026-05-31 20:40:37 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,13 @@
 // Copyright 2021 NNTU-CS
 #include <iostream>
-#include <string>
 #include "bst.h"
 
-void makeTree(BST<std::string>& tree, const char* fname);
+void makeTree(BST<std::string>& tree, const char* filename);
 void printFreq(BST<std::string>& tree);
 
 int main() {
     BST<std::string> tree;
-
     makeTree(tree, "src/war_peace.txt");
-
-    if (tree.empty()) {
-        std::cout << "No words found!" << std::endl;
-        return 1;
-    }
-
     printFreq(tree);
-
     return 0;
 }
